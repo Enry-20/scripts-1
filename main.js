473256a3d1b33b4ba3e7111c4f6e72c9ad2f2100
Enry-20 <enriquecuellar7u7@gmail.com>
2020-10-19 03:54:40 +0100

    physics commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector(".nav-ligas").classList.toggle("show");
+  });
+  
+//   ScrollReveal().reveal('.showcase');
+//   ScrollReveal().reveal('.news-cards', { delay: 500 });
+//   ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+//   ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
